db1032c2005d52c6482f4de643d0e78b326346c6
R-tech11 <76420799+R-tech11@users.noreply.github.com>
2021-10-31 20:33:14 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,111 @@
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+var score = 0;
+
+function preload()
+{
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
+}
+
+
+function setup()
+{
+  createCanvas(400,400);
+
+  // Moving background
+  garden = createSprite(200,200);
+  garden.addImage(gardenImg);
+
+  //creating rabbit running
+  rabbit = createSprite(160,340,20,20);
+  rabbit.scale = 0.09;
+  rabbit.addImage(rabbitImg);
+
+  textSize(20);
+}
+
+function draw() 
+{
+  background(0);
+  
+  //rabbit moving on Xaxis with mouse'
+  if(rabbit.x >= World.mouseX + 50 && World.mouseY >= rabbit.y)
+  {
+    rabbit.x = rabbit.x + 10;
+  }
+  else if(rabbit.x <= World.mouseX + 50 && World.mouseY >= rabbit.y)
+  {
+    rabbit.x = rabbit.x - 10;
+  }
+  
+  if(rabbit.x >= World.mouseX || rabbit.x <= mouseX)
+  {
+    rabbit.x = rabbit.x + 0;
+  }
+
+  edges = createEdgeSprites();
+  rabbit.collide(edges);
+  
+  drawSprites();
+
+  var select_sprites = Math.round(random(1,3));
+
+  if (frameCount % 80 == 0) 
+  {
+    if (select_sprites == 1) 
+    {
+      createApples();
+    } 
+    else if (select_sprites == 2) 
+    {
+      createOrange();
+    }
+    else 
+    {
+      createRed();
+    }
+  }
+}
+
+function createApples() 
+{
+  apple = createSprite(random(50, 350),40, 10, 10);
+  apple.addImage(appleImg);
+  apple.scale = 0.07;
+  apple.velocityY = 3;
+  apple.lifetime = 125;
+}
+
+function createOrange() 
+{
+  orangeL = createSprite(random(50, 350),40, 10, 10);
+  orangeL.addImage(orangeImg);
+  orangeL.scale = 0.08;
+  orangeL.velocityY = 3;
+  orangeL.lifetime = 125;
+}
+
+function createRed() 
+{
+  redL = createSprite(random(50, 350),40, 10, 10);
+  redL.addImage(redImg);
+  redL.scale = 0.06;
+  redL.velocityY = 3;
+  redL.lifetime = 125;
+}
+
+// The code I try to give score (it didn't work).
+
+// if(rabbit.collide(apple))
+// {
+//   score = score + 1;
+// }
+// else if(rabbit.colldie(orangeL) || rabbit.colldie(orangeL))
+// {
+//   score = score - 1;
+// }
+
